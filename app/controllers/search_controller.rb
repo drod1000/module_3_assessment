cb70e873b84b4807ea2cebb7f0392e4f2fba3729
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 11:17:24 -0700

    Stores showing up in view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,8 @@ class SearchController < ApplicationController
     zip_code = params[:zip_code]
     response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip_code},25))?format=json&show=storeType,name,city,distance,phone&pageSize=10&apiKey=#{ENV["BEST_BUY_API_KEY"]}"
     parsed = JSON.parse(response.body, symbolize_names: true)[:stores]
+    @stores = parsed
+    binding.pry
   end
 end
 
