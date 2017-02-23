c4c1afdc7375861690f12b631988e76aa8d94c9d
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 09:50:20 -0700

    Improves tests for items index and show

--- spec/requests/items_api_spec.rb
+++ spec/requests/items_api_spec.rb
@@ -2,22 +2,24 @@ require "rails_helper"
 
 describe "Items API" do
   it "can return all items" do
+    create_list(:item, 3)
     get '/api/v1/items'
 
     expect(response).to be_success
 
-    items = JSON.parse(response.body)
-    expect(items).to be_an(Array)
+    found_items = JSON.parse(response.body)
+    expect(found_items).to be_an(Array)
+    expect(found_items.count).to eq(3)
 
-    item = items.first
+    found_item = found_items.first
 
-    expect(item).to be_a(Hash)
-    expect(item).to have_key("id")
-    expect(item).to have_key("name")
-    expect(item).to have_key("description")
-    expect(item).to have_key("image_url")
-    expect(item).to_not have_key("created_at")
-    expect(item).to_not have_key("updated_at")
+    expect(found_item).to be_a(Hash)
+    expect(found_item).to have_key("id")
+    expect(found_item).to have_key("name")
+    expect(found_item).to have_key("description")
+    expect(found_item).to have_key("image_url")
+    expect(found_item).to_not have_key("created_at")
+    expect(found_item).to_not have_key("updated_at")
   end
 
   it "can return a single item" do
@@ -32,7 +34,7 @@ describe "Items API" do
     expect(found_item["name"]).to eq(item.name)
     expect(found_item["description"]).to eq(item.description)
     expect(found_item["image_url"]).to eq(item.image_url)
-    expect(item).to_not have_key("created_at")
-    expect(item).to_not have_key("updated_at")
+    expect(found_item).to_not have_key("created_at")
+    expect(found_item).to_not have_key("updated_at")
   end
 end
