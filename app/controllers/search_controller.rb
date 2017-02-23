07e0257b50615c678bb8fb6a86a3058d5e6e661e
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 11:24:05 -0700

    Search test passing: too many instance variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,10 @@
 class SearchController < ApplicationController
   def index
-    zip_code = params[:zip_code]
-    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip_code},25))?format=json&show=storeType,name,city,distance,phone&pageSize=10&apiKey=#{ENV["BEST_BUY_API_KEY"]}"
-    parsed = JSON.parse(response.body, symbolize_names: true)[:stores]
-    @stores = parsed
+    @zip_code = params[:zip_code]
+    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{@zip_code},25))?format=json&show=storeType,name,city,distance,phone&pageSize=10&apiKey=#{ENV["BEST_BUY_API_KEY"]}"
+    binding.pry
+    @parsed = JSON.parse(response.body, symbolize_names: true)
+    @stores = @parsed[:stores]
     binding.pry
   end
 end
