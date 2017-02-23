db174eae1025c454d95a68094ffb18dfeb6f6198
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 10:00:53 -0700

    Adds test for creating an item

--- spec/requests/items_api_spec.rb
+++ spec/requests/items_api_spec.rb
@@ -9,6 +9,7 @@ describe "Items API" do
     expect(response).to be_success
 
     found_items = JSON.parse(response.body)
+
     expect(found_items).to be_an(Array)
     expect(found_items.count).to eq(3)
 
@@ -47,5 +48,20 @@ describe "Items API" do
 
     expect(response).to be_success
     expect(Item.count).to eq(0)
+  end
+
+  it "can create an item" do
+    post "/api/v1/items?name=some_name&description=some_description&image_url=some_image"
+
+    expect(response).to be_success
+
+    returned_item = JSON.parse(response.body)
+
+    expect(returned_item["id"]).to eq(1)
+    expect(returned_item["name"]).to eq("some_name")
+    expect(returned_item["description"]).to eq("some_description")
+    expect(returned_item["image_url"]).to eq("some_image")
+    expect(returned_item).to_not have_key("created_at")
+    expect(returned_item).to_not have_key("updated_at")
   end
 end
