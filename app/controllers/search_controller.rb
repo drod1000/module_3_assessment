4c1cf9f066daf03a120dfcec539cb89b6d3090a7
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 11:09:58 -0700

    Successfully getting response from BB API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,10 @@
 class SearchController < ApplicationController
   def index
-    binding.pry
+    zip_code = params[:zip_code]
+    response = Faraday.get "https://api.bestbuy.com/v1/stores(area(#{zip_code},25))?format=json&show=storeType,name,city,distance,phone&pageSize=10&apiKey=#{ENV["BEST_BUY_API_KEY"]}"
+    parsed = JSON.parse(response.body, symbolize_names: true)[:stores]
   end
 end
+
+
+# https://api.bestbuy.com/v1/stores(area(zip_code,25))?format=json&show=storeType,name,city,distance,phone&pageSize=10&apiKey=ENV[BEST_BUY_API_KEY]
--- spec/features/user_searches_for_nearby_stores_spec.rb
+++ spec/features/user_searches_for_nearby_stores_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 describe "When a user visit '/'" do
   it "they can search for stores near their zip code" do
-    visit '/'
+    VCR.use_cassette("user searches for stores") do
+      visit '/'
 
-    fill_in 'zip_code', :with => '80202'
-    click_on("Search")
+      fill_in 'zip_code', :with => '80202'
+      click_on("Search")
 
-    expect(current_path).to eq('/search')
-    expect(page).to have_content("Stores within 25 miles of 80202")
-    expect(page).to have_content("16 Total Stores")
-    expect(page).to have_content("Long Name:")
-    expect(page).to have_content("City:")
-    expect(page).to have_content("Distance:")
-    expect(page).to have_content("Phone Number:")
-    expect(page).to have_content("Store Type:")
+      expect(current_path).to eq('/search')
+      expect(page).to have_content("Stores within 25 miles of 80202")
+      expect(page).to have_content("16 Total Stores")
+      expect(page).to have_content("Long Name:")
+      expect(page).to have_content("City:")
+      expect(page).to have_content("Distance:")
+      expect(page).to have_content("Phone Number:")
+      expect(page).to have_content("Store Type:")
+    end
 
     #When I visit "/"
     # And I fill in a search box with "80202" and click "search"
