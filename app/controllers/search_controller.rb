9c5c2827f07b1dc715d59b90dbef82abdda235d9
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 11:44:26 -0700

    Only two instance variables now

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
     @zip_code = params[:zip_code]
-    @parsed = BestBuyService.get_stores(@zip_code)
-    @stores = @parsed[:stores]
+    @response = BestBuyService.get_stores(@zip_code)
   end
 end
