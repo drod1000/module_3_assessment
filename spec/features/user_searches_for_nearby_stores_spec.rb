f3685602f2914bb0ae537ff2cfb4980f1c8e4bd9
Daniel Rodriguez <drod2793@gmail.com>
2017-02-23 11:40:35 -0700

    Removes commented out story

--- spec/features/user_searches_for_nearby_stores_spec.rb
+++ spec/features/user_searches_for_nearby_stores_spec.rb
@@ -17,13 +17,5 @@ describe "When a user visit '/'" do
       expect(page).to have_content("Phone Number:")
       expect(page).to have_content("Store Type:")
     end
-
-    #When I visit "/"
-    # And I fill in a search box with "80202" and click "search"
-    # Then my current path should be "/search" (ignoring params)
-    # And I should see stores within 25 miles of 80202
-    # And I should see a message that says "16 Total Stores"
-    # And I should see exactly 10 results
-    # And I should see the long name, city, distance, phone number and store type for each of the 10 results
   end
 end
